9565ec0aafd472c86a8bcb3f662857f4aea11c4b
tootal <tootal@yeah.net>
2019-12-10 13:10:46 +0800

    add call action

--- app/src/main/java/xyz/tootal/contactsystem/PersonActivity.java
+++ app/src/main/java/xyz/tootal/contactsystem/PersonActivity.java
@@ -1,14 +1,24 @@
 package xyz.tootal.contactsystem;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -17,15 +27,17 @@ import org.w3c.dom.Text;
 public class PersonActivity extends AppCompatActivity {
 
     public static final String PERSON_NAME = "person_name";
-
+    private Person person;
     public static final String PERSON_IMAGE_URI = "person_image_uri";
+    private ImageButton person_msg_button;
+    private ImageButton person_call_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
         Intent intent=getIntent();
-        Person person=(Person)intent.getSerializableExtra("person");
+        person=(Person)intent.getSerializableExtra("person");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         ImageView personImageView = (ImageView) findViewById(R.id.person_image_view);
@@ -42,6 +54,27 @@ public class PersonActivity extends AppCompatActivity {
 
         person_number_textview.setText(person.getNumber());
         personContentText.setText(person.getNote());
+
+        person_call_button=(ImageButton) findViewById(R.id.person_call_button);
+        person_msg_button=(ImageButton) findViewById(R.id.person_msg_button);
+
+        person_call_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(PersonActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(PersonActivity.this, new String[]{ Manifest.permission.CALL_PHONE }, 1);
+                } else {
+                    callPhone(person.getNumber());
+                }
+            }
+        });
+
+        person_msg_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 
@@ -54,4 +87,25 @@ public class PersonActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    public void callPhone(String phoneNum){
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        Uri data = Uri.parse("tel:" + phoneNum);
+        intent.setData(data);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    callPhone(person.getNumber());
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+
 }
